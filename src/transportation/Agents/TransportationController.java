23ca49f3c507338d93d0d35c881cc13158172e4e
Daron Lee <daronlee@usc.edu>
2013-11-23 19:36:53 -0800

    Messages for people completed

--- src/transportation/Agents/TransportationController.java
+++ src/transportation/Agents/TransportationController.java
@@ -20,6 +20,20 @@ public class TransportationController extends Agent {
 		TransportationState transportationState;
 		PersonAgent person;
 		MobileAgent mobile;
+		
+		String startingLocation;
+		String endingLocation;
+		String method;
+		String character;
+		
+		Mover(PersonAgent person, String startingLocation, String endingLocation, String method, String character) {
+			this.person = person;
+			this.startingLocation = startingLocation;
+			this.endingLocation = endingLocation;
+			this.method = method;
+			this.character = character;
+			transportationState = TransportationState.REQUEST;
+		}
 	}
 
 	List<Mover> movingObjects;
@@ -44,13 +58,29 @@ public class TransportationController extends Agent {
 		movingObjects = new ArrayList<Mover>();
 		directory = new HashMap<String, Building>();
 	}
-
+	
+	//+++++++++++++++++MESSAGES+++++++++++++++++
+	public void msgWantToGo(String startLocation, String endLocation, PersonAgent person, String mover, String character) {
+		movingObjects.add(new Mover(person, startLocation, endLocation, mover, character));
+		stateChanged();
+	}
+	
+	public void msgArrivedAtDestination(PersonAgent person){
+		for(Mover mover : movingObjects) {
+			if(mover.person == person) {
+				mover.transportationState = TransportationState.DESTINATION;
+			}
+		}
+		stateChanged();
+	}
+	
+	//+++++++++++++++++SCHEDULER+++++++++++++++++
 	@Override
 	protected boolean pickAndExecuteAnAction() {
 		synchronized(movingObjects) {
 			for(Mover mover : movingObjects) {
 				if(mover.transportationState == TransportationState.REQUEST) {
-					SpawnVehicle(mover);
+					spawnMover(mover);
 					return true;
 				}
 			}
@@ -59,18 +89,49 @@ public class TransportationController extends Agent {
 		synchronized(movingObjects) {
 			for(Mover mover : movingObjects) {
 				if(mover.transportationState == TransportationState.DESTINATION) {
-					
+					despawnMover(mover);//POTENTIAL ERROR
+					return true;
 				}
 			}
 		}
 		
 		synchronized(movingObjects) {
+			boolean retry = false;
 			for(Mover mover : movingObjects) {
 				if(mover.transportationState == TransportationState.WAITINGTOSPAWN) {
-					
+					retry = true;
+					retrySpawn(mover);
 				}
 			}
+			if(retry)
+				return true;
 		}
 		return false;
+	}
+	
+	private void spawnMover(Mover mover) {
+		//Try to spawn mover
+		switch(mover.method) {
+			case "car":
+				//spawn car
+				break;
+			
+			case  "walk":
+				//spawn walker
+				break;
+			
+			case "bus":
+				//find bus stop and spawn walker to go to bus stop
+				break;
+		}
+	}
+	
+	private void despawnMover(Mover mover) {
+		mover.person.msgReachedDestination(mover.endingLocation);
+		movingObjects.remove(mover);
+	}
+	
+	private void retrySpawn(Mover mover) {
+		mover.transportationState = TransportationState.REQUEST;
 	}
 }
