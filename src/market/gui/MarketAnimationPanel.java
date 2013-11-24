74eba548cde4ccf1a8f8706f48bb8434f6532a3f
Erika Johnson <erikajoh@usc.edu>
2013-11-24 05:56:12 -0800

    updated market animation

--- src/market/gui/MarketAnimationPanel.java
+++ src/market/gui/MarketAnimationPanel.java
@@ -27,8 +27,6 @@ public class MarketAnimationPanel extends JPanel implements ActionListener {
     Timer timer = new Timer(5, this);
     Graphics2D g2;
     
-    private JButton kitchen = new JButton(" ");
-
     public MarketAnimationPanel() {
     	this.setSize(400, 360);
         setVisible(true);
@@ -55,18 +53,17 @@ public class MarketAnimationPanel extends JPanel implements ActionListener {
         g2.setColor(getBackground());
         g2.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-//        g2.setColor(Color.GRAY);
-//        g2.fillRect((int)(this.getWidth()*0.5), (int)(this.getHeight()*0.2), this.getWidth()/50, this.getHeight()/5);
-//        g2.fillRect((int)(this.getWidth()*0.6), (int)(this.getHeight()*0.93), (int)(this.getWidth()*0.4), (int)(this.getHeight()*0.9));
-//        g2.fillRect((int)(this.getWidth()*0.95), (int)(this.getHeight()*0.6), (int)(this.getWidth()*0.2), (int)(this.getHeight()*0.4));
-//        g2.fillRect((int)(this.getWidth()*0.15), (int)(this.getHeight()*0.6), (int)(this.getWidth()*0.2), (int)(this.getHeight()*0.15));
-//
-//        g2.setColor(Color.BLACK);
-//        g2.draw(new Line2D.Double(3*this.getWidth()/5, this.getHeight()/2, this.getWidth(), this.getHeight()/2));
-//        g2.draw(new Line2D.Double(this.getWidth()/2, 0, this.getWidth()/2, this.getHeight()/2));
-//        g2.draw(new Line2D.Double(this.getWidth()/4, (int)(this.getHeight()*0.95), this.getWidth()/4, this.getHeight()));
-//        g2.draw(new Line2D.Double(this.getWidth()/3, (int)(this.getHeight()*0.95), this.getWidth()/3, this.getHeight()));
-//        
+        g2.setColor(Color.GRAY);
+        g2.fillRect((int)(this.getWidth()*0.15), (int)(this.getHeight()*0.4), (int)(this.getWidth()*0.075), (int)(this.getHeight()*0.2));
+        g2.fillRect((int)(this.getWidth()*0.4), (int)(this.getHeight()*0.4), (int)(this.getWidth()*0.1), (int)(this.getHeight()*0.7));
+        g2.fillRect((int)(this.getWidth()*0.65), (int)(this.getHeight()*0.4), (int)(this.getWidth()*0.1), (int)(this.getHeight()*0.7));
+        g2.fillRect((int)(this.getWidth()*0.9), (int)(this.getHeight()*0.4), (int)(this.getWidth()*0.1), (int)(this.getHeight()*0.7));
+
+        g2.setColor(Color.BLACK);
+        g2.draw(new Line2D.Double((int)(this.getWidth()*0.25), 0, (int)(this.getWidth()*0.25), this.getHeight()));
+        g2.draw(new Line2D.Double((int)(this.getWidth()*0.25), this.getHeight()/4, this.getWidth(), this.getHeight()/4));
+        g2.draw(new Line2D.Double(2*this.getWidth()/3, 0, 2*this.getWidth()/3, this.getHeight()/4));
+        
         for(Gui gui : guis) {
             if (gui.isPresent()) {
                 gui.updatePosition();
