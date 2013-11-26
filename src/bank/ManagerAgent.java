c56068a10aa520731a27ba9d53e344a1168d88b2
Erika Johnson <erikajoh@usc.edu>
2013-11-26 07:58:35 -0800

    another bank fix

--- src/bank/ManagerAgent.java
+++ src/bank/ManagerAgent.java
@@ -222,16 +222,16 @@ public class ManagerAgent extends Agent implements Manager {
 	private void assignTeller(MyTeller mt, WaitingCustomer wc){	
 		wc.bankCustomer.msgGoToTeller(mt.teller);
 		print(wc.toString() + " " + wc.action);
-//		if(wc.action == Action.newAccount){
-//			wc.bankCustomer.msgRequestNewAccount(wc.requestAmt);
-//		}
-//		else if(wc.action == Action.deposit){
-//			wc.bankCustomer.msgRequestDeposit(wc.requestAmt, wc.accountNum);
-//		}
-//		else if(wc.action == Action.withdraw){
-//			print("RA: "+wc.requestAmt);
-//			wc.bankCustomer.msgRequestWithdraw(wc.requestAmt, wc.accountNum);
-//		}
+		if(wc.action == Action.newAccount){
+			wc.bankCustomer.msgRequestNewAccount(wc.requestAmt);
+		}
+		else if(wc.action == Action.deposit){
+			wc.bankCustomer.msgRequestDeposit(wc.requestAmt, wc.accountNum);
+		}
+		else if(wc.action == Action.withdraw){
+			print("RA: "+wc.requestAmt);
+			wc.bankCustomer.msgRequestWithdraw(wc.requestAmt, wc.accountNum);
+		}
 
 		wc.state = State.idle;
 		mt.teller.msgNewCustomer(wc.bankCustomer);
