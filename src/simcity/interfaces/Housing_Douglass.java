d54718e45d67cb31e270a1e3af399069f1dbff86
Douglass Chen <douglasschen@gmail.com>
2013-11-26 16:30:10 -0800

    More unit test setup required, merge for fixing transportation animation

--- src/simcity/interfaces/Housing_Douglass.java
+++ src/simcity/interfaces/Housing_Douglass.java
@@ -1,15 +1,35 @@
 package simcity.interfaces;
 
+import housing.ResidentAgent;
 import java.util.Map;
 
 import simcity.PersonAgent;
 
 public interface Housing_Douglass {
 	public String getName();
+	
 	public void msgHereIsRent(PersonAgent personAgent, double amount);
+	
 	public void msgIAmHome(PersonAgent personAgent);
+	
 	public void msgIAmLeaving(PersonAgent personAgent);
+	
 	public void msgGoToBed(PersonAgent personAgent);
-	public void msgPrepareToCookAtHome(PersonAgent personAgent,
-			String foodPreference);
+	
+	public void msgPrepareToCookAtHome(PersonAgent personAgent, String foodPreference);
+	
+	public void msgIAmHome(PersonAgent rp, Map<String, Integer> items);
+	
+	public void msgDoMaintenance();
+	
+	public void msgRentDue();
+	
+	public void msgEntered(ResidentAgent ra);
+	
+	public void msgFinishedMaintenance(ResidentAgent ra);
+	
+	public void msgFoodDone(ResidentAgent ra, boolean success);
+	
+	public void msgLeft(ResidentAgent ra);
+	
 }
