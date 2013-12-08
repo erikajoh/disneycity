766d3793e67d5836a37a40ccbb9a5e2edd948587
Daron Lee <daronlee@usc.edu>
2013-12-07 18:30:47 -0800

    Making commit so I can edit something in master

--- src/transportation/Objects/TrafficLight.java
+++ src/transportation/Objects/TrafficLight.java
@@ -3,6 +3,7 @@ package transportation.Objects;
 import astar.astar.Position;
 
 public class TrafficLight {
+	//ALWAYS PLACE TRAFFIC LIGHT IN THE TOP LEFT GRID IN THE CENTER OF THE INTERSECTION OR THE TRANSPORTATION WILL BREAK HORRIBLY
 	Position location;
 	MovementTile[][] grid;
 }
